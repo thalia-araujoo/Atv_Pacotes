53a9f912998159e057d07794fd27b67618b54631
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-11-23 16:49:37 -0300

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,36 @@
+package br.edu.ifce.biblioteca.model;
+
+
+public class Usuario {
+    private String Nome;
+    private int Id;
+    private int Ano;
+    String LivrosEmprestados[] = new LivrosEmprestados[];
+
+    public void String getLivrosEmprestados(){
+        return this.LivrosEmprestados[];
+    }
+    
+    public void String setNome{
+        this.Nome=Nome;
+    }
+    public String getNome(){
+        return this.Nome;
+    }
+
+    public void int setId{
+        this.Id=Id;
+    }
+    public int getId(){
+        return this.Id;
+    }
+
+    public void int setAno{
+        this.Ano=Ano;
+    }
+    public int getAno(){
+        return this.Ano;
+    }
+
+    
+}
